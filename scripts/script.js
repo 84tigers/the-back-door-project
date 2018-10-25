f179cac4e0e6fae44917d453fbf26565335bee7a
Lauren <laurewallace@gmail.com>
2018-10-25 13:14:26 -0400

    Added the checkout section with functionality for the shopping cart button, the checkout button, and the close button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 $(document).ready(() => {
-
+    $(".cart").hide()
+    $(".payment").hide()
     $(document)
     .on("click", ".fa-bars", (event) => {
         $(".drop-menu").css("display", "flex");
@@ -12,6 +13,16 @@ $(document).ready(() => {
         $(".drop-menu").hide();
         $(event.target).hide();
         $(".fa-bars").show();
+    })
+    .on("click", ".shopping-cart", (event) => {
+        $(".cart").show()
+    })
+    .on("click", ("#close-btn"), (event) => {
+        $(".cart").hide();
+    })
+    .on("click", ".checkout", (event) => {
+        $(".cart").hide();
+        $(".payment").show();
     })
 
 
